41ccf7be4766afec84266c5c32fc9b7dd325c06e
Anatholyb1 <anatholyb@gmail.com>
2024-01-04 18:23:31 +0700

    yc

--- e_commerce_store/e_commerce_store/doctype/storefront_website_settings/storefront_website_settings.py
+++ e_commerce_store/e_commerce_store/doctype/storefront_website_settings/storefront_website_settings.py
@@ -3,12 +3,8 @@
 
 # import frappe
 from frappe.model.document import Document
-import frappe
+
 
 class StorefrontWebsiteSettings(Document):
 	pass
 
-
-def get_website_settings():
-	website_settings = frappe.get_all("Shipping Rule", filters={"custom_show_on_website": 1}, fields=["name","shipping_rule_type","shipping_amount"])
-	return website_settings
