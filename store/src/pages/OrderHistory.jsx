cbf86e9ffe640c6a0dbf992fc728e754e2bef52b
MarkSuttirak <suttirak.ch@zaviago.com>
2024-01-05 11:15:24 +0000

    Update CSS codes on order history page

--- store/src/pages/OrderHistory.jsx
+++ store/src/pages/OrderHistory.jsx
@@ -2,6 +2,7 @@ import { useOrder } from "../hooks/useOrders";
 import { useState, useEffect } from "react";
 import { SfThumbnail, SfLoaderCircular, SfSelect } from "@storefront-ui/react";
 import { Link } from "react-router-dom";
+import MyAccountSection from "../components/MyAccountSection";
 
 const month = {
     0: "Jan",
@@ -40,39 +41,70 @@ function OrderHistory() {
     : Order.filter(item => item.status === selectedStatus);
 
     return (
-        <main className="main-section">
-            <h1 className="mb-8 primary-heading text-primary text-center">Order history</h1>
-            <section className="grid grid-cols-1 lg:grid-cols-3 lg:gap-10">
-                <div className="col-span-1">
-                    <label className="typography-text-sm font-medium mb-2 block">Show</label>
+        <MyAccountSection>
+            <h1 className="primary-heading text-primary mb-8">Order history</h1>
+            <div className="flex flex-col gap-y-4">
+              <div className="flex border-b">
+                {statusOptions.map(option => (
+                    <button onClick={() => setSelectedStatus(option)} key={option} className={`px-4 py-2 border-b-2 ${selectedStatus === option ? 'border-b-black' : 'border-b-white'}`}>{option}</button>
+                ))}
+            </div>
+            <div className="flex items-center gap-x-2">
+                    <label className="typography-text-sm font-medium">Show</label>
                     <SfSelect name="state" placeholder="-- Select --" onChange={(e) => setSelectedShow(e.target.value)}>
                         {showList.map((list, index) => (
                             <option key={index} value={list === 'All' ? filteredData.length : list}>{list === 'All' ? 'All orders' : `Last ${list} orders`}</option>
                         ))}
                     </SfSelect>
                 </div>
-                <div className="col-span-2 flex flex-col gap-y-4">
-                    <div className="flex border-b">
-                        {statusOptions.map(option => (
-                            <button onClick={() => setSelectedStatus(option)} key={option} className={`px-4 py-2 border-b-2 ${selectedStatus === option ? 'border-b-black' : 'border-b-white'}`}>{option}</button>
-                        ))}
+            {!loading ? filteredData.slice(0, selectedShow).map(({name, status, base_total, company, items, creation}) => (
+                <Link to={`/order-history/${name}`} key={name} className="grid grid-cols-5">
+                    <div className="grid grid-cols-5 gap-2 col-span-3">
+                    <SfThumbnail size="lg" className="bg-gray-100"/>
+                    <div className="col-span-4">
+                        <h2 className="font-bold">{name}-{}{company}</h2>
+                        <p>{`${new Date(creation).getDate()} ${month[new Date(creation).getMonth()]}, ${new Date(creation).getHours()}:${new Date(creation).getMinutes() < 10 ? '0' + new Date(creation).getMinutes(): new Date(creation).getMinutes() }  `}</p>
                     </div>
-                    {!loading ? filteredData.slice(0, selectedShow).map(({name, status, base_total, company, items, creation}) => (
-                        <Link to={`/order-history/${name}`} key={name} className="grid grid-cols-5">
-                          <div className="grid grid-cols-5 gap-2 col-span-3">
-                            <SfThumbnail size="lg" className="bg-gray-100"/>
-                            <div className="col-span-4">
-                              <h2 className="font-bold">{name}-{}{company}</h2>
-                              <p>{`${new Date(creation).getDate()} ${month[new Date(creation).getMonth()]}, ${new Date(creation).getHours()}:${new Date(creation).getMinutes() < 10 ? '0' + new Date(creation).getMinutes(): new Date(creation).getMinutes() }  `}</p>
-                            </div>
-                          </div>
-                          <p>{status}</p>
-                          <span className="text-right">฿{base_total}</span>
-                        </Link>
-                    ))  : <SfLoaderCircular/>}
+                    </div>
+                    <p>{status}</p>
+                    <span className="text-right">฿{base_total}</span>
+                </Link>
+            ))  : <SfLoaderCircular/>}
                 </div>
-            </section>
-        </main>
+        </MyAccountSection>
+        // <main className="main-section">
+        //     <h1 className="mb-8 primary-heading text-primary text-center">Order history</h1>
+        //     <section className="grid grid-cols-1 lg:grid-cols-3 lg:gap-10">
+        //         <div className="col-span-1">
+        //             <label className="typography-text-sm font-medium mb-2 block">Show</label>
+        //             <SfSelect name="state" placeholder="-- Select --" onChange={(e) => setSelectedShow(e.target.value)}>
+        //                 {showList.map((list, index) => (
+        //                     <option key={index} value={list === 'All' ? filteredData.length : list}>{list === 'All' ? 'All orders' : `Last ${list} orders`}</option>
+        //                 ))}
+        //             </SfSelect>
+        //         </div>
+        //         <div className="col-span-2 flex flex-col gap-y-4">
+        //             <div className="flex border-b">
+        //                 {statusOptions.map(option => (
+        //                     <button onClick={() => setSelectedStatus(option)} key={option} className={`px-4 py-2 border-b-2 ${selectedStatus === option ? 'border-b-black' : 'border-b-white'}`}>{option}</button>
+        //                 ))}
+        //             </div>
+        //             {!loading ? filteredData.slice(0, selectedShow).map(({name, status, base_total, company, items, creation}) => (
+        //                 <Link to={`/order-history/${name}`} key={name} className="grid grid-cols-5">
+        //                   <div className="grid grid-cols-5 gap-2 col-span-3">
+        //                     <SfThumbnail size="lg" className="bg-gray-100"/>
+        //                     <div className="col-span-4">
+        //                       <h2 className="font-bold">{name}-{}{company}</h2>
+        //                       <p>{`${new Date(creation).getDate()} ${month[new Date(creation).getMonth()]}, ${new Date(creation).getHours()}:${new Date(creation).getMinutes() < 10 ? '0' + new Date(creation).getMinutes(): new Date(creation).getMinutes() }  `}</p>
+        //                     </div>
+        //                   </div>
+        //                   <p>{status}</p>
+        //                   <span className="text-right">฿{base_total}</span>
+        //                 </Link>
+        //             ))  : <SfLoaderCircular/>}
+        //         </div>
+        //     </section>
+        // </main>
     );
 }
 
--- store/src/pages/Profile.jsx
+++ store/src/pages/Profile.jsx
@@ -1,34 +1,15 @@
 import React, { useState } from 'react'
 import AddressForm from '../components/forms/AddressForm'
-import ProfileCard from '../components/ProfileCard'
 import AddressListing from '../components/AddressListing'
-import OrderHistory from '../components/OrderHistory'
-import { Link } from 'react-router-dom'
+import MyAccountSection from '../components/MyAccountSection'
 
 const Profile = () => {
     const [randomKey, setrandomKey] = useState(0)
-    const menus = [
-      {title:'My Orders', link:'/order-history'},
-      {title:'My Account', link:'/profile'}
-    ]
     return (
-      <main className='main-section'>
-        <div className='grid grid-cols-1 lg:grid-cols-3 gap-x-10'>
-          <div className='lg:col-span-1'>
-            <ProfileCard />
-            {/* <div className='flex flex-col gap-y-2'>
-              {menus.map(menu => (
-                <Link to={menu.link} className={`px-4 py-2 rounded-md hover:bg-gray-100`}>{menu.title}</Link>
-              ))}
-            </div> */}
-            <OrderHistory className="sm:block hidden"/>
-          </div>
-          <div className='lg:col-span-2'>
-            <AddressForm onSuccess={() => setrandomKey(randomKey + 1)} />
-            <AddressListing randomKey={randomKey} />
-          </div>
-        </div>
-      </main>
+      <MyAccountSection>
+        <AddressForm onSuccess={() => setrandomKey(randomKey + 1)} />
+        <AddressListing randomKey={randomKey} />
+      </MyAccountSection>
     )
 }
 
