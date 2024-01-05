3a5b6032dbae890041b1f247f441e2089ae07996
Anatholyb1 <anatholyb@gmail.com>
2024-01-05 09:22:21 +0700

    add store front settings

---
+++ e_commerce_store/e_commerce_store/doctype/storefront_settings/storefront_settings.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2024, Zaviago and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class StorefrontSettings(Document):
+	pass
