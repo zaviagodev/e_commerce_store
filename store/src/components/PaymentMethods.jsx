f43235e4ff2add3c0abca4d87804ff208f98b409
Nabeel Goraya <itsgoraya@gmail.com>
2024-01-28 01:51:50 +0500

    payment updates

--- store/src/components/PaymentMethods.jsx
+++ store/src/components/PaymentMethods.jsx
@@ -1,6 +1,9 @@
 // List of payment methods
 import qrmock from '../assets/qrmock.png'
+import { useState} from 'react';
 import { Skeleton } from './Skeleton';
+import { useFrappeGetCall, useFrappePostCall } from 'frappe-react-sdk';
+
 const paymentMethods = [
     {
         label: 'QR พร้อมเพย์',
@@ -19,17 +22,22 @@ export default function PaymentMethods({
     value,
     error
 }) {
+    const [randomKey, setrandomKey] = useState(0)
+    const { data:paymentmethods } = useFrappeGetCall('webshop.webshop.api.payment_info', null, `payments-${randomKey}`)
+
+    console.log(paymentmethods);
+
     return (
         <>
-            {paymentMethods ? (
+            {paymentmethods ? (
                 <fieldset className="w-full">
                     <legend className="mb-2 font-semibold text-secgray">วิธีการชำระเงิน</legend>
-                    <div className={`grid ${paymentMethods?.length == 2 ? 'grid-cols-2' : 'grid-cols-1'} gap-4 items-stretch`}>
-                        {paymentMethods.map(({ label, value: nameVal, logo }) => (
-                            <label key={nameVal} className="relative" onClick={() => onChange(nameVal)}>
-                                <div className={`flex ${paymentMethods?.length > 1 ? 'flex-col gap-y-2' : 'items-center gap-x-2'} px-4 py-3 cursor-pointer rounded-xl border border-lightgray -outline-offset-2 hover:border-lightgray hover:bg-primary-100 peer-focus:border-primary-200 peer-focus:bg-primary-100 bg-neutral-50 ${value == nameVal ? "border-primary-300 outline outline-[1px]" : ""}`}>
+                    <div className={`grid ${paymentmethods?.length == 2 ? 'grid-cols-2' : 'grid-cols-1'} gap-4 items-stretch`}>
+                        {paymentmethods.message.map(({name,key }) => (
+                            <label key={name} className="relative" onClick={() => onChange(key)}>
+                                <div className={`flex ${paymentmethods?.length > 1 ? 'flex-col gap-y-2' : 'items-center gap-x-2'} px-4 py-3 cursor-pointer rounded-xl border border-lightgray -outline-offset-2 hover:border-lightgray hover:bg-primary-100 peer-focus:border-primary-200 peer-focus:bg-primary-100 bg-neutral-50 ${value == key ? "border-primary-300 outline outline-[1px]" : ""}`}>
                                     {/* <img src={logo} alt={label} className="h-5 w-5 select-none object-cover rounded-[2px]" /> */}
-                                    <p className={`text-darkgray  font-bold text-center`}>{label}</p>
+                                    <p className={`text-darkgray  font-bold text-center`}>{name}</p>
                                 </div>
                             </label>
                         ))}
--- store/src/pages/BankInfoPage.jsx
+++ store/src/pages/BankInfoPage.jsx
@@ -1,13 +1,46 @@
 import { SfButton } from '@storefront-ui/react';
-import React from 'react'
+import { useState,React} from 'react';
 import { Link, useNavigate, useSearchParams } from 'react-router-dom';
 import defaultLogo from '../assets/defaultBrandIcon.svg';
 import { useSetting } from '../hooks/useWebsiteSettings';
+import { useFrappeGetCall } from 'frappe-react-sdk';
+
 
 const BankInfoPage = () => {
     const [searchParams] = useSearchParams();
     const navigate = useNavigate()
     const { appLogo } = useSetting()
+    const payment_method = searchParams.get("payment_method");
+    const [Order, setOrder] = useState([])
+    const [randomKey, setrandomKey] = useState(0)
+    const [Pagestep, setPagestep] = useState(0)
+    const [paymentinfo, setpaymentinfo] = useState(0)
+
+    const { mutate: mutateOrder, error: orderError } = useFrappeGetCall('webshop.webshop.api.get_orders', null, `orders-${randomKey}`,{
+          isOnline: () => Order.length === 0,
+          onSuccess: (data) => {
+            data.message?.map((dd) => {
+              if (dd.name == searchParams.get("order_id")) {
+                setOrder(dd);
+              }
+              return null;
+            });
+          },
+        }
+    );
+    const { data:paymentmethods } = useFrappeGetCall('webshop.webshop.api.payment_info', null, `payments-${randomKey}`,{
+        isOnline: () => Order.length === 0,
+        onSuccess: (data) => {
+          data.message?.map((dd) => {
+            if (dd.key == searchParams.get("payment_method")) {
+                setpaymentinfo(dd);
+            }
+            return null;
+          });
+        },
+      }
+  );
+
 
     const data = [
       { title:'Order ID', info:searchParams.get("order_id")},
@@ -31,19 +64,80 @@ const BankInfoPage = () => {
                 <div className='flex flex-col gap-y-[26px] py-4'>
                     <h1 className='text-lg text-center font-medium'>ใบเสร็จ บริษัท ซาเวียโก</h1>
                     <div className='flex flex-col gap-y-[14px]'>
-                        {data.map(d => (
-                            <div className='flex items-center justify-center gap-x-1'>
-                                <h2 className='text-sm font-medium'>{d.title}: </h2>
-                                <p className='text-sm'>{d.info}</p>
-                            </div>
-                        ))}
+
+                    {console.log(paymentinfo)}
+                    {payment_method == 1 && (
+                        <>
+                            {Pagestep === 0 && (
+                                <>
+                                    <h1 className='text-lg text-center font-medium'>Setp 1 Order Deatils</h1>
+                                    <div className='flex items-center justify-center gap-x-1'>
+                                        <h1>{Order.grand_total}</h1>
+                                    </div>
+                                    <div className='flex items-center justify-center gap-x-1'>
+                                        {/* Render HTML content using dangerouslySetInnerHTML */}
+                                        <div dangerouslySetInnerHTML={{ __html: Order.address_display }} />
+                                    </div>
+
+                                    {Order.items?.map((d, index) => (
+                                        <div key={index} className='flex items-center justify-center gap-x-1'>
+                                            <h2 className='text-sm font-medium'>{d.item_name}: </h2>
+                                            <p className='text-sm'>{d.rate}</p>
+                                        </div>
+                                    ))}
+
+                                    <SfButton variant='tertiary' className='w-full btn-primary' onClick={() => setPagestep(1)}>
+                                        PayNow
+                                    </SfButton>
+                                    <div>Your content for payment_method 1 and Pagestep 1</div>
+                                </>
+                            )}
+
+                            {Pagestep === 1 && (
+                                
+                                <>
+                                    <h1 className='text-lg text-center font-medium'>Setp 2 QR Deatils</h1>
+                                    <div className='flex items-center justify-center gap-x-1'>
+                                        <h2 className='text-sm font-medium'>Sales Invoice: </h2>
+                                        <p className='text-sm'>123123123</p>
+                                    </div>
+
+                                    <div className='flex items-center justify-center gap-x-1'>
+                                        <img
+                                            src={paymentinfo.promptpay_qr_image ? `${import.meta.env.VITE_ERP_URL ?? ''}${paymentinfo.promptpay_qr_image}` : ""}
+                                            alt="Sf Logo"
+                                        />
+                                    </div>
+                                    {paymentinfo.account_name && (
+                                        <div className='flex items-center justify-center gap-x-1'>
+                                            <h2 className='text-sm font-medium'>Account Name </h2>
+                                            <p className='text-sm'>{paymentinfo.account_name}</p>
+                                        </div>
+                                    )}
+
+                                    {paymentinfo.promptpay_number && (
+                                        <div className='flex items-center justify-center gap-x-1'>
+                                            <h2 className='text-sm font-medium'>Account Number </h2>
+                                            <p className='text-sm'>{paymentinfo.promptpay_number}</p>
+                                        </div>
+                                    )}
+
+                                    <SfButton variant='tertiary' className='w-full btn-primary' onClick={() => setPagestep(2)}>
+                                        PayNow
+                                    </SfButton>
+                                    <div>Your content for payment_method 1 and Pagestep 2</div>
+                                </>
+                            )}
+                        </>
+                    )}
+
+
+
+
+
                     </div>
                 </div>
-                <Link to='/order-history'>
-                    <SfButton variant='tertiary' className='w-full btn-primary'>
-                        Go to My orders
-                    </SfButton>
-                </Link>
+               
             </div>
         </div>
     )
