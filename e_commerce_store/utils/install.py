8f9c9ae66f8fc0f76f0490eff3be0518c0bb2c02
AnatholyB1 <anatholy.bricon@viacesi.fr>
2024-02-02 13:12:50 +0700

    feat(core): merge headless with e_comme

--- e_commerce_store/utils/install.py
+++ e_commerce_store/utils/install.py
@@ -44,6 +44,17 @@ def after_install():
     
     add_permissions()
 
+    frappe.get_single("Webshop Settings").update({
+        "products_per_page": 100,
+        "show_price": 1,
+        "show_stock_availability": 1,
+        "allow_items_not_in_stock": 1,
+        "enabled": 1,
+        "price_list": "Standard Selling",
+        "quotation_series": "SAL-QTN-.YYYY.-",
+        "default_customer_group": "All Customer Groups",
+    }).save()
+
 def add_permissions():
     roles = [d for d in install_docs if d.get("doctype") == "Role"]
     for role in roles:
--- store/src/pages/LoyaltyProgram.jsx
+++ store/src/pages/LoyaltyProgram.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { useFrappeGetCall } from 'frappe-react-sdk';
 
 const LoyaltyProgram = () => {
-    const { data } = useFrappeGetCall('headless_e_commerce.api.get_loyalty_points_details')
+    const { data } = useFrappeGetCall('e_commerce_store.api.get_loyalty_points_details')
     return (
         <div className="px-4 m-3 md:px-6 md:shadow-lg md:rounded-md md:border md:border-neutral-100">
             <p className="text-2xl my-3 font-bold">Points History</p>
