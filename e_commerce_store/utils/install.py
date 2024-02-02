092d59ab33790470284990629373423d740cb7fc
AnatholyB1 <anatholy.bricon@viacesi.fr>
2024-02-02 13:16:47 +0700

    feat(core):merge backend from headless

--- e_commerce_store/utils/install.py
+++ e_commerce_store/utils/install.py
@@ -43,6 +43,16 @@ def after_install():
             pass
     
     add_permissions()
+    frappe.get_single("Webshop Settings").update({
+        "products_per_page": 100,
+        "show_price": 1,
+        "show_stock_availability": 1,
+        "allow_items_not_in_stock": 1,
+        "enabled": 1,
+        "price_list": "Standard Selling",
+        "quotation_series": "SAL-QTN-.YYYY.-",
+        "default_customer_group": "All Customer Groups",
+    }).save()
 
     frappe.get_single("Webshop Settings").update({
         "products_per_page": 100,
