def7e4d16ab156b3a6027002983c99c064d6b7aa
umer2001 <umer2001.uf@gmail.com>
2024-07-09 13:05:18 +0500

    feat: hide or show close buton on sheet

--- store/src/components/customComponents/TopSheet.tsx
+++ store/src/components/customComponents/TopSheet.tsx
@@ -7,35 +7,35 @@ import {
   SheetTitle,
   SheetTrigger,
 } from "@/components/ui/sheet";
-import { ReactNode } from "react"
+import { ReactNode } from "react";
 
 type SearchbarProps = {
-  trigger: string | ReactNode
-  children: ReactNode
-  onOpenChange?: (open: boolean) => void
-  open?: boolean
-  triggerClassName?: string
-  contentClassName?: string
-}
+  trigger: string | ReactNode;
+  children: ReactNode;
+  onOpenChange?: (open: boolean) => void;
+  open?: boolean;
+  triggerClassName?: string;
+  contentClassName?: string;
+  contentProps?: any;
+};
 
-const TopSheet = ({ 
+const TopSheet = ({
   trigger,
   children,
   onOpenChange,
   open,
   triggerClassName,
-  contentClassName
-} : SearchbarProps) => {
+  contentClassName,
+  contentProps,
+}: SearchbarProps) => {
   return (
     <Sheet onOpenChange={onOpenChange} open={open}>
-      <SheetTrigger className={triggerClassName}>
-        {trigger}
-      </SheetTrigger>
-      <SheetContent side="top" className={contentClassName}>
+      <SheetTrigger className={triggerClassName}>{trigger}</SheetTrigger>
+      <SheetContent side="top" className={contentClassName} {...contentProps}>
         {children}
       </SheetContent>
     </Sheet>
-  )
-}
+  );
+};
 
-export default TopSheet
+export default TopSheet;
